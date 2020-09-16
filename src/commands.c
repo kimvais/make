2ca650fd75bb26eb7508e12c5f1671522b3d357a
Kimmo Parviainen-Jalanko <kimvais@kimva.is>
2020-09-16 12:02:51 +0300

    Applying CTRL-C hang patch.

--- src/commands.c
+++ src/commands.c
@@ -475,6 +475,12 @@ execute_file_commands (struct file *file)
   /* Start the commands running.  */
   new_job (file);
 }
+
+#ifdef WINDOWS32
+/* defined in job.c */
+extern void wait_until_main_thread_sleeps (void);
+#endif
+
 
 /* This is set while we are inside fatal_error_signal,
    so things can avoid nonreentrant operations.  */
@@ -511,20 +517,7 @@ fatal_error_signal (int sig)
   /* Windows creates a separate thread for handling Ctrl+C, so we need
      to suspend the main thread, or else we will have race conditions
      when both threads call reap_children.  */
-  if (main_thread)
-    {
-      DWORD susp_count = SuspendThread (main_thread);
-
-      if (susp_count != 0)
-        fprintf (stderr, "SuspendThread: suspend count = %ld\n", susp_count);
-      else if (susp_count == (DWORD)-1)
-        {
-          DWORD ierr = GetLastError ();
-
-          fprintf (stderr, "SuspendThread: error %ld: %s\n",
-                   ierr, map_windows32_error_to_string (ierr));
-        }
-    }
+  wait_until_main_thread_sleeps();
 #endif
   handling_fatal_signal = 1;
 
@@ -588,8 +581,6 @@ fatal_error_signal (int sig)
 #endif
 
 #ifdef WINDOWS32
-  if (main_thread)
-    CloseHandle (main_thread);
   /* Cannot call W32_kill with a pid (it needs a handle).  The exit
      status of 130 emulates what happens in Bash.  */
   exit (130);
--- src/main.c
+++ src/main.c
@@ -264,6 +264,12 @@ static const int inf_jobs = 0;
 
 static char *jobserver_auth = NULL;
 
+#ifdef WINDOWS32
+/* defined in job.c */
+extern void create_susp_main_event (void);
+extern void delete_susp_main_event (void);
+#endif
+
 /* Handle for the mutex used on Windows to synchronize output of our
    children under -O.  */
 
@@ -1065,6 +1071,7 @@ main (int argc, char **argv, char **envp)
   const char *windows32_path = NULL;
 
   SetUnhandledExceptionFilter (handle_runtime_exceptions);
+  create_susp_main_event ();
 
   /* start off assuming we have no shell */
   unixy_shell = 0;
@@ -3512,6 +3519,10 @@ die (int status)
           int _x UNUSED;
           _x = chdir (directory_before_chdir);
         }
+
+#ifdef WINDOWS32
+      delete_susp_main_event ();
+#endif
     }
 
   exit (status);
